59f0a187f5ac42c2da274e9f423ea2dfb0436274
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-04-12 18:23:39 +0200

    14-binary_tree_balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,7 +9,7 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height = 0;
+	size_t left_height, right_height = 0;
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);

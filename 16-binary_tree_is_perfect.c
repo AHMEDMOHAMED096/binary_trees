3cfafccf02338f1b0e9049ec5ce2dffa323e9148
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-04-12 05:03:47 +0200

    16-binary_tree_is_perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,11 +11,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int left_height, right_height;
 
-	if (tree == NULL)
-		return (0);
-
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
+
+	if (tree == NULL)
+		return (0);
 
 	if (left_height == right_height)
 	{
